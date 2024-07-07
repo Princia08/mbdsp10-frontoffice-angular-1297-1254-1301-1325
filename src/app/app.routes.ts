9311c551c060f13b693f1f89f7d36b80d2d18462
Princia08 <princiaram38@yahoo.com>
2024-07-07 22:53:37 +0300

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/authentification/login/login.component';
+import { InscriptionComponent } from './pages/authentification/inscription/inscription.component';
 
 export const routes: Routes = [
     { path : '', component : LoginComponent },
+    { path : 'signup', component : InscriptionComponent },
 ];
